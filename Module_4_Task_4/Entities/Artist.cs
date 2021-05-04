53b64569444371964d8d9cebcd324cb8e931e733
meridian34 <sobol.andrey.25@gmail.com>
2021-05-04 16:00:33 +0300

    Add entities

---
+++ Module_4_Task_4/Entities/Artist.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module_4_Task_4.Entities
+{
+    public class Artist
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public string InstagramUrl { get; set; }
+    }
+}
---
+++ Module_4_Task_4/Entities/Song.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module_4_Task_4.Entities
+{
+    public class Song
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Duration { get; set; }
+        public DateTime RelesedDate { get; set; }
+    }
+}
