2d838003c2dab863d52c6d8dff9d4b4596d2b844
meridian34 <sobol.andrey.25@gmail.com>
2021-05-04 17:55:22 +0300

    fix property for table

--- Module_4_Task_4/EntityConfigurations/SongConfiguration.cs
+++ Module_4_Task_4/EntityConfigurations/SongConfiguration.cs
@@ -16,7 +16,7 @@ namespace Modul_4_Task_4.EntityConfigurations
             builder.ToTable("Song").HasKey(s => s.Id);
             builder.Property(s => s.Duration).HasColumnName("Duration").IsRequired();
             builder.Property(s => s.RelesedDate).HasColumnName("RelesedDate").HasColumnType("date");
-            builder.Property(s => s.Title).HasColumnName("Title").HasMaxLength(50);
+            builder.Property(s => s.Title).HasColumnName("Title").HasMaxLength(50).IsRequired();
 
             builder.HasOne(s => s.Genre)
                 .WithMany(g => g.Songs)
